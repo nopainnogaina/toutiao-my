bff2436344d35a9582faf6f62ecb8d3454dd68c3
木子 <1805489519@qq.com>
2020-11-02 13:31:45 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- 路由的出口不能删 -->
+    <router-view/>
+    <h1>黑马头条</h1>
+    <i class="iconfont toutiao-gengduo"></i>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    
+    <!--单元格-->
+<van-cell-group>
+  <van-cell title="单元格" value="内容" />
+  <van-cell title="单元格" value="内容" label="描述信息" />
+</van-cell-group>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name:'app'
+}
+</script>
+
+
+<style lang="less">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 加载全局样式
+import './styles/icon.less'
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
+
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  
+  render: h => h(App)
+}).$mount('#app')
