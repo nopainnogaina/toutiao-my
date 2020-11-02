c7ac729c8dc7abda5ec6f1cae06a6d7a344a1d75
木子 <1805489519@qq.com>
2020-11-02 15:10:14 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 
 // 加载全局样式
 import './styles/icon.less'
-// 加载 Vant 核心组件库
+// 加载 Vant 核心组件库 
 import Vant from 'vant'
 
 // 加载 Vant 全局样式
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 
 
 Vue.use(VueRouter)
-
+// 路由表
   const routes = []
 
 const router = new VueRouter({
